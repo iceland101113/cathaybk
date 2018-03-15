b15c892260f95ac44315c3a232e646709d37e693
Dex15900122 <e5900122@yahoo.com.tw>
2018-03-15 18:14:41 +0800

    add basic view

--- app/controllers/cathaybk_controller.rb
+++ app/controllers/cathaybk_controller.rb
@@ -2,4 +2,8 @@ class CathaybkController < ApplicationController
   def index
     @select = Select.first
   end
+
+  def basic
+    @select = Select.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "basic", to: "cathaybk#basic"
   get "cathaybk", to: "cathaybk#index"
   resources :phone_numbers, only: [:new, :create]
   post 'phone_numbers/verify' => "phone_numbers#verify"
 
   root "phone_numbers#new"
+
 
 end
