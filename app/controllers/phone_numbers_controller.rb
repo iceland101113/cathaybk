c5d2bfdbf4a8d76afcca2f61b45756db62186f71
Dex15900122 <e5900122@yahoo.com.tw>
2018-04-03 15:33:32 +0800

    limit 3 times at same number

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -2,26 +2,33 @@ class PhoneNumbersController < ApplicationController
 
   def new
     @phone_number = PhoneNumber.new
-
+    
   end
 
   def create
     @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number][:phone_number])
-    if @phone_number.save
-      @phone_number.generate_pin
-      @phone_number.send_pin
-      session[:phone_number] = @phone_number
     
-      respond_to do |format|
-        
-        format.js # render app/views/phone_numbers/create.js.erb
-      end
-    
+    if PhoneNumber.where("phone_number = ?" , @phone_number.phone_number.to_s).size >= 3
+      redirect_to root_path, notice:'登入操過次數'
     else
-     
-      render :new, notice:'填入號碼有誤'
+
+      if @phone_number.save
+        @phone_number.generate_pin
+        @phone_number.send_pin
+        session[:phone_number] = @phone_number
       
+        respond_to do |format|
+          
+          format.js # render app/views/phone_numbers/create.js.erb
+        end
+      
+      else
+        render :new, notice:'填入號碼有誤' 
+      end
+
     end
+      
+    
     
   end
 
@@ -31,7 +38,7 @@ class PhoneNumbersController < ApplicationController
     if @phone_number.verified == true
       redirect_to basic_path, notice:'驗證成功'
     else
-      render :new, notice:'填入號碼有誤'
+      redirect_to root_path, notice:'填入號碼有誤'
     end
 
   end
