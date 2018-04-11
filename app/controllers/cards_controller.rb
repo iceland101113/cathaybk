6899503ecd8ab488c53b2268d8c07be83f93ddd2
Morrie <morriedog@hotmail.com>
2018-04-12 02:15:00 +0800

    use current_phone to replace the repeat get session[:phone]

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,30 +2,23 @@ class CardsController < ApplicationController
 
   before_action :authenticate_user!
   # before_action :authorize
-
-
   before_action :set_card, only: [:update, :destroy, :take]
 
 
   def index
     @cards = Card.all
-    
-
   end
+
   def create
     @card = Card.new(card_params)
     if @card.save
-  
       redirect_to cards_path
-
     else
-      
       render :index
     end
   end
 
   def update
-    
     if @card.update(card_params)
       redirect_to cards_path
     else
@@ -37,8 +30,8 @@ class CardsController < ApplicationController
   def show
     @cards = Card.all
     @card = Card.find(params[:id])
-    @phone_number = session[:phone_number]
-    @yournumber = TakeLog.today.find_by(ip_address: @phone_number["phone_number"] ,card_id: @card)
+    # @phone_number = session[:phone_number]
+    @yournumber = TakeLog.today.find_by(ip_address: current_phone.phone_number ,card_id: @card)
     
     if @card.over?
       redirect_to cards_path, notice: "時段額滿"
@@ -46,7 +39,6 @@ class CardsController < ApplicationController
     # if @card.points > 6
     #   redirect_to cards_path, notice: "時段額滿"
     # end
-    
     if @yournumber != nil
         @yournumber = @yournumber.take_count 
     end
@@ -63,23 +55,17 @@ class CardsController < ApplicationController
     end
 
     def take
-      
-      @phone_number = session[:phone_number]
-      @yournumber = @card.take_logs.create(ip_address: @phone_number["phone_number"], take_count: @card.take_logs.size+1) 
+      @yournumber = @card.take_logs.create(ip_address: current_phone.phone_number, take_count: @card.take_logs.size+1) 
       unless @yournumber == nil
         message = "您的號碼是: #{@yournumber.take_count}
 
                    時段: #{@card.title}"
-        # @phone = PhoneNumber.find(@phone_number["id"])
-        # @phone.send_message(@phone.phone_number, message)
+        # @phone.send_message(current_phone.phone_number, message)
 
 
                  
         ContactMailer.say_hello_to(current_user,message).deliver_now
 
-         
-
-        # TwilioTextMessenger.new(message).call
       end
       redirect_to cards_path, notice: "預約成功,請看簡訊或者信箱"
     end
--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -3,26 +3,20 @@ class PhoneNumbersController < ApplicationController
   def new
     @phone_number = PhoneNumber.new
     @pin = rand(0000..9999).to_s.rjust(4, "0")
-        
-        @phone_number.pin = @pin
-        session[:pin] = @pin
+    @phone_number.pin = @pin
+    session[:pin] = @pin
   end
 
   def create
     @phone_number = PhoneNumber.create(phone_number: params[:phone_number][:phone_number])
-    
-
     if PhoneNumber.where("phone_number = ?" , @phone_number.phone_number).size >= 3
       redirect_to root_path, notice:'登入操過次數'
-
     else
-
       if @phone_number.save
         # @phone_number.generate_pin
         # @phone_number.send_pin
-        session[:phone_number] = @phone_number
-        
-        
+        # session[:phone_number] = @phone_number
+        login(@phone_number)
         respond_to do |format|
           
           format.js # render app/views/phone_numbers/create.js.erb
