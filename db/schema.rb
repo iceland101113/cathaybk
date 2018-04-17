7618bd7a53052cda79b01ea1fba4a368bc119228
iceland101113 <r02521115@ntu.edu.tw>
2018-04-17 21:45:23 +0800

    change card view style and fix form problem

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180416154550) do
 
-  create_table "address_lanlngs", force: :cascade do |t|
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "addresslanlngs", force: :cascade do |t|
     t.string "address"
     t.datetime "created_at", null: false
