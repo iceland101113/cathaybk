80c80ba956e9cbb4d75c2d45ea515d1658146c6e
Dex15900122 <e5900122@yahoo.com.tw>
2018-07-25 12:26:58 +0800

    add seo

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,7 +3,7 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://www.dexicelandmorrie.us/'
 SitemapGenerator::Sitemap.create do
-  add "/phone numbers", :changefreq => 'daily', :priority => 0.9
+  add '/basic' , :changefreq => 'daily', :priority => 0.9
   # add '/contact_us', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
