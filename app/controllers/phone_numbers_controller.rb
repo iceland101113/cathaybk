0b1d35ad09be226c2034780a4cd5bd54b46acafb
Dex15900122 <e5900122@yahoo.com.tw>
2018-07-25 11:40:26 +0800

    add seo

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -41,10 +41,12 @@ class PhoneNumbersController < ApplicationController
   end
 
 
+  
   def robots
-    robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
-    render :text => robots, :layout => false, :content_type => "text/plain"
+    robots = File.read(Rails.root.join('config', "robots.#{Rails.env}.txt"))
+    render plain: robots
   end
+
 
   private
 
